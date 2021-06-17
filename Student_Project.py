cf1991bd37695a0a3eca30c2588c8eb9708bb38f
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-06-17 10:13:10 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,13 @@
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+
+
+def addition(n1,n2):
+    print(n1+n2)
+def subtraction(n1,n2):
+    print(n1-n2)
+#Create functions for multiplication and division
+    
+addition(n1,n2)
+subtraction(n1,n2)
+#Call both the functions and pass parameters n1, n2
